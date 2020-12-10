2a836e3415220bfddbc459c1cf008352a8ba2e85
Katitu Katituson <other.katitu@gmail.com>
2020-12-10 14:56:50 +0100

    g

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/katitu/Documents/GitHub/wordpress-nginx-docker
+# Build directory: C:/Users/katitu/Documents/GitHub/wordpress-nginx-docker/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
